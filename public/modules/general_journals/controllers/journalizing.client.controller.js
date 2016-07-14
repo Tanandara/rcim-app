e5a569dbfedcf97214cdd57f3a465bb924b9fc78
Tanandara <tanandara2014@gmail.com>
2016-07-14 23:36:09 +0700

    update jounalizing

--- public/modules/general_journals/controllers/journalizing.client.controller.js
+++ public/modules/general_journals/controllers/journalizing.client.controller.js
@@ -1,25 +1,41 @@
 angular.module("general_journals").controller("JournalizingController",
 ["$scope","$http",
 function($scope,$http){
-$scope.date = new Date();
 
+$scope.details = [];
 $scope.getJournal = function(){
-    $http({
-      method: 'GET',
-      url: $scope.dbURL + '/general_journals'
-    }).success(function(data, status) {
-      $scope.journal_details = data;
-    });
+    // $http({
+    //   method: 'GET',
+    //   url: $scope.dbURL + '/general_journals'
+    // }).success(function(data, status) {
+    //   $scope.journal_details = data;
+    // });
+}
+
+
+$scope.addDetail = function(){
+    var transaction =   {
+                          "detail":$scope.detail,
+                          "ledger_id":$scope.ledger_id || "X01",
+                          "drcr":$scope.drcr,
+                          "amount":$scope.amount
+                        }
+    $scope.details.push(transaction);
+    // clear ค่าทิ้ง
+    $scope.detail = "";
+    $scope.ledger_id  = "";
+    $scope.drcr = "";
+    $scope.amount = "";
+
 }
 
 
 
 $scope.SumDrCr = function(drcr){
   var sum = 0;
-    angular.forEach($scope.journal_details,function(item,index){
+    angular.forEach($scope.details,function(item,index){
       if(item.drcr==drcr){
         sum += ((parseFloat(item.amount).toFixed(2))/1);
-        console.log(sum);
       }
     });
     return sum;
