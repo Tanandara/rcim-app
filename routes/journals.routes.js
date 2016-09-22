12582729c45c03841c2948c52fb56f22e6bcad1f
Tanandara <tanandara2014@gmail.com>
2016-09-23 05:05:42 +0700

    fix test

--- routes/journals.routes.js
+++ routes/journals.routes.js
@@ -3,5 +3,4 @@ module.exports = function(app){
   var queries = require("../controllers/queries");
   app.post("/journals/add",journals.CheckRefNo,journals.AddJournals);
   app.post("/journal_detail",journals.ShowDetails);
-  app.post("/journals/test",journals.test,journals.test2);
 }
