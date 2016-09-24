8b6e8bb489a23cf1172f7df4da695f86d340294f
Tanandara <tanandara2014@gmail.com>
2016-09-24 16:31:57 +0700

    remark http get

--- public/modules/general_journals/controllers/searchrefno.client.controller.js
+++ public/modules/general_journals/controllers/searchrefno.client.controller.js
@@ -3,12 +3,12 @@ angular.module("general_journals").controller("SearchRefNoController",
 function($scope,$http,$state){
 
   $scope.getJournals = function(){
-      $http({
-        method: 'GET',
-        url: $scope.dbURL + '/journals'
-      }).success(function(data, status) {
-        $scope.journals = data;
-      });
+      // $http({
+      //   method: 'GET',
+      //   url: $scope.dbURL + '/journals'
+      // }).success(function(data, status) {
+      //   $scope.journals = data;
+      // });
   }
 
   $scope.goDetail = function(){
