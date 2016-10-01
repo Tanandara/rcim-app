e052363a3dc22661d6251457f3626f5905e53029
Tanandara <tanandara2014@gmail.com>
2016-10-01 20:03:55 +0700

    fix query

--- controllers/ledgers.controller.js
+++ controllers/ledgers.controller.js
@@ -46,51 +46,53 @@ exports.BroughtForward = function(req,res){
     req.body.coa_id = (req.body.coa_id).replace(/0{6}$|0{4}$|0{2}$/,"");
     models.sequelize.query(
       `
-      select
-      (
-      	select
-      	sum(amount) as sumDr
-      	from
-      	(
-      		SELECT
-      		a.coa_id,
-      		a.date_time,
-      		a.ref_no,
-      		b.coa_detail as detail,
-      		a.drcr,
-      		sum(a.amount) as amount
-      		FROM journals as a
-      		left outer join
-      		(select ref_no,coa_detail from journals where drcr = 3) as b
-      		on a.ref_no = b.ref_no
-      		where a.coa_id regexp :coa_id and a.account_id = :account_id and a.date_time < :datestart and a.drcr = 1
-      		group by date_time,ref_no,detail,drcr
-      		order by a.date_time
-      	) as sumDr
-      )
-      -
-      (
-      	select
-      	sum(amount) as sumCr
-      	from
-      	(
-      		SELECT
-      		a.coa_id,
-      		a.date_time,
-      		a.ref_no,
-      		b.coa_detail as detail,
-      		a.drcr,
-      		sum(a.amount) as amount
-      		FROM journals as a
-      		left outer join
-      		(select ref_no,coa_detail from journals where drcr = 3) as b
-      		on a.ref_no = b.ref_no
-      		where a.coa_id regexp :coa_id and a.account_id = :account_id and a.date_time < :datestart and a.drcr = 2
-      		group by date_time,ref_no,detail,drcr
-      		order by a.date_time
-      	) as sumCr
-      )
-      as brought_forward
+      select Dr - Cr as brought_forward from
+        (
+        	select if(sumDr.sum is null , 0 , sumDr.sum) as Dr,if(sumCr.sum is null , 0 , sumCr.sum) as Cr from
+        		  (
+        			select
+        			sum(amount) as sum
+        			from
+        			(
+        					SELECT
+        					a.coa_id,
+        					a.date_time,
+        					a.ref_no,
+        					b.coa_detail as detail,
+        					a.drcr,
+        					sum(a.amount) as amount
+        					FROM journals as a
+        					left outer join
+        					(select ref_no,coa_detail from journals where drcr = 3) as b
+        					on a.ref_no = b.ref_no
+        					where a.coa_id regexp :coa_id and a.account_id = :account_id and a.date_time < :datestart and a.drcr = 1
+        					group by date_time,ref_no,detail,drcr
+        					order by a.date_time
+        			) as sumDr
+        		  ) as sumDr
+        		 cross join
+        		 (
+        			select
+        			sum(amount) as sum
+        			from
+        			(
+        					SELECT
+        					a.coa_id,
+        					a.date_time,
+        					a.ref_no,
+        					b.coa_detail as detail,
+        					a.drcr,
+        					sum(a.amount) as amount
+        					FROM journals as a
+        					left outer join
+        					(select ref_no,coa_detail from journals where drcr = 3) as b
+        					on a.ref_no = b.ref_no
+        					where a.coa_id regexp :coa_id and a.account_id = :account_id and a.date_time < :datestart and a.drcr = 2
+        					group by date_time,ref_no,detail,drcr
+        					order by a.date_time
+        			) as sumCr
+        		 ) as sumCr
+        ) as SumAll
       `
       ,
       {
